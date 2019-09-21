a477422c4a2191a459ca5fd172a54ac8d4edd83e
Samuel Nuttall <sndrummer@gmail.com>
2019-09-20 21:10:24 -0600

    changed logger format

--- BinaryMinHeap/src/Logger.h
+++ BinaryMinHeap/src/Logger.h
@@ -9,17 +9,17 @@ public:
 
     ~Logger();
 
-    static void log_info(const char *message);
+    static void log_info(const char* message);
 
-    static void log_info(const std::string &message);
+    static void log_info(const std::string& message);
 
-    static void log_debug(const char *message);
+    static void log_debug(const char* message);
 
-    static void log_debug(const std::string &message);
+    static void log_debug(const std::string& message);
 
-    static void log_error(const char *message);
+    static void log_error(const char* message);
 
-    static void log_error(const std::string &message);
+    static void log_error(const std::string& message);
 
 
 #ifndef NDEBUG
